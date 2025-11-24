83f3e47358ec48d0d8171aa7f27d574635ed5a60
wallif-portifolio <ruan.wallif@academico.ifpb.edu.br>
2025-11-24 19:08:58 -0300

    Delete ProjetoPOO/src directory

--- ProjetoPOO/src/dao/InterfacePersistence.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import java.util.List;
-
-public interface InterfacePersistence<T> {
-    void salvar(List<T> lista);
-    List<T> carregar();
-}
--- ProjetoPOO/src/exception/ReservaInvalidaException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class ReservaInvalidaException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-	public ReservaInvalidaException(String msg) {
-        super(msg);
-    }
-}
